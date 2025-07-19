07bf7f965e32cfbfb5456cb0a7b706a12beedfdd
omniimarket <proedgemarket@gmail.com>
2025-07-18 21:16:40 -0700

    FEAT: Integrated Google Analytics into _document.js and removed conflicting _document.ts

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,6 +1,6 @@
 // pages/_document.js
 // This file customizes the initial server-side rendered document.
-// It's used to load global resources like fonts efficiently.
+// It's used to load global resources like fonts and Google Analytics efficiently.
 
 import { Html, Head, Main, NextScript } from 'next/document';
 
@@ -10,7 +10,21 @@ export default function Document() {
       <Head>
         {/* Load Inter font globally here */}
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-        {/* You can also add other global meta tags or links here if needed */}
+
+        {/* Google tag (gtag.js) - Google Analytics GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7ZM060P22W"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-7ZM060P22W');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
       </Head>
       <body>
         <Main />
